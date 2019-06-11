bed0ab66ee7b6b115ebf3bb91fb78b88100b2255
DiegoRamires <goncalves.ramires@gmail.com>
2019-06-11 15:38:23 -0300

    install and enable cors

--- src/index.js
+++ src/index.js
@@ -1,11 +1,14 @@
 const express = require('express')
 const mongoose = require('mongoose')
 const path = require('path')
+const cors = require('cors')
 
 const app = express()
 mongoose.connect('mongodb+srv://astro-9:star@cluster0-ahjnp.mongodb.net/test?retryWrites=true&w=majority', {
   useNewUrlParser: true,
 })
+
+app.use(cors())
 
 app.use('/files', express.static(path.resolve(__dirname, '..', 'uploads', 'resized')))
 
