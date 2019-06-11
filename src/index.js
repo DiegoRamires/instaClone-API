50c2b4437f00a166d9e0bb596089d64f47c4b092
DiegoRamires <goncalves.ramires@gmail.com>
2019-06-11 13:48:28 -0300

    install mongoose and add db connection

--- src/index.js
+++ src/index.js
@@ -1,6 +1,10 @@
 const express = require('express')
+const mongoose = require('mongoose')
 
 const app = express()
+mongoose.connect('mongodb+srv://astro-9:star@cluster0-ahjnp.mongodb.net/test?retryWrites=true&w=majority', {
+  useNewUrlParser: true,
+})
 
 app.get('/', (req, res) => {
   return res.send('Hello World')
