c02f25d8e7a9caacb606e5d5f7a55816962a9e45
DiegoRamires <goncalves.ramires@gmail.com>
2019-06-12 23:49:56 -0300

    update server configuration

--- src/index.js
+++ src/index.js
@@ -24,6 +24,8 @@ app.use('/files', express.static(path.resolve(__dirname, '..', 'uploads', 'resiz
 
 app.use(require('./routes'))
 
-app.listen(3000)
-console.log('Server running on localhost:3000')
-console.log('Hit `ctrl + c` to stop server')
+server.listen(process.env.PORT || 3001, process.env.IP, function() {
+  console.log("The App server is running!")
+  console.log("Access http://localhost:3001 on development mode")
+  console.log("Hit ctrl + c to stop!")
+})
